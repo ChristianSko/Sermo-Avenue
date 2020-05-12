7512a88b87905804d017d6bf3abda1de08997496
Ivan Tilev <iivantilev@gmail.com>
2020-05-12 15:23:58 +0200

    Initial Commit

---
+++ AlphaBeez/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AlphaBeez
+//
+//  Created by Ivan Tilev on 12/05/2020.
+//  Copyright © 2020 Ivan Tilev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
