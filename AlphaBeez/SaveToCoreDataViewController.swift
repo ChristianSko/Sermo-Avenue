c1ca35fff252ab12c3b097b86826a22892c2ef5e
Ivan Tilev <iivantilev@gmail.com>
2020-05-24 00:10:36 +0200

    Setting up the textFields so they can accept input and pass this input into the values ( validated by printing to the console )

--- AlphaBeez/SaveToCoreDataViewController.swift
+++ AlphaBeez/SaveToCoreDataViewController.swift
@@ -12,7 +12,7 @@ import CoreData
 // MARK: - Here we can create new items and save them to the CoreData Model
 
 // ( will be visible only for developer stuff only and the user will not have access to it! )
-class SaveToCoreDataViewController: UIViewController {
+class SaveToCoreDataViewController: UIViewController, UITextFieldDelegate {
     
     // Outlets
     @IBOutlet weak var nameTextField: UITextField!
@@ -22,32 +22,42 @@ class SaveToCoreDataViewController: UIViewController {
     @IBOutlet weak var syllablesTextField: UITextField!
     
     // Properties to keep the value from the textFields
-    var name = String()
-    var category = String()
-    var image = String()
-    var hapticPath = String()
-    var syllables = Int()
+    var name: String = ""
+    var category: String = ""
+    var image: String = ""
+    var hapticPath: String = ""
+    var syllables: Int = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        // Setting this ViewController to be the delegate for all the textFields
+        nameTextField.delegate = self
+        categoryTextField.delegate = self
+        imageTextField.delegate = self
+        hapticTextField.delegate = self
+        syllablesTextField.delegate = self
     }
     
     // MARK: - Actions
     @IBAction func saveNameButtonPressed(_ sender: UIButton) {
+        print(name)
     }
     
     @IBAction func saveCategoryButtonPressed(_ sender: UIButton) {
+        print(category)
     }
     
     @IBAction func saveImageButtonPressed(_ sender: UIButton) {
+        print(image)
     }
     
     @IBAction func saveHapticButtonPressed(_ sender: UIButton) {
+        print(hapticPath)
     }
     
     @IBAction func saveSyllablesButtonPressed(_ sender: UIButton) {
+        print(syllables)
     }
     
     
@@ -57,5 +67,31 @@ class SaveToCoreDataViewController: UIViewController {
     @IBAction func saveFlashcardPressed(_ sender: UIBarButtonItem) {
     }
     
+    // Will determine which texField is typing on and will assign the value to the proper property
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        switch textField {
+        case nameTextField:
+            name = nameTextField.text!
+        case categoryTextField:
+            category = categoryTextField.text!
+        case imageTextField:
+            image = imageTextField.text!
+        case hapticTextField:
+            hapticPath = hapticTextField.text!
+        case syllablesTextField:
+            syllables = Int(syllablesTextField.text!)!
+        default:
+            break
+        }
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        nameTextField.resignFirstResponder()
+        categoryTextField.resignFirstResponder()
+        imageTextField.resignFirstResponder()
+        hapticTextField.resignFirstResponder()
+        syllablesTextField.resignFirstResponder()
+        return true
+    }
     
 }
