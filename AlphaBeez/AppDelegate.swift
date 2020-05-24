da57f7f055a03c34215b39e6499b6a3d19837369
Ivan Tilev <iivantilev@gmail.com>
2020-05-24 15:31:14 +0200

    Adding the AHAPs for the Park Category to the app

--- AlphaBeez/AppDelegate.swift
+++ AlphaBeez/AppDelegate.swift
@@ -20,6 +20,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         let hapticCapability = CHHapticEngine.capabilitiesForHardware()
         supportsHaptics = hapticCapability.supportsHaptics
         
+        print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))
+        
         return true
     }
 
