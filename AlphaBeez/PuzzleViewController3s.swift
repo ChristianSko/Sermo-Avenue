c468ec42dfae9f3a26feee94aa362196447c9ce4
Christian Skorobogatow <skorome@icloud.com>
2020-05-25 23:46:03 +0200

    Indented Code

--- AlphaBeez/PuzzleViewController3s.swift
+++ AlphaBeez/PuzzleViewController3s.swift
@@ -11,7 +11,7 @@ import UIKit
 class PuzzleViewController3s: UIViewController {
     
     
-     //Image Outlets
+    //Image Outlets
     @IBOutlet weak var rightPuzzlePiece: UIImageView!
     @IBOutlet weak var upperLeftPuzzlePiece: UIImageView!
     @IBOutlet weak var bottomLeftPuzzlePiece: UIImageView!
@@ -25,10 +25,10 @@ class PuzzleViewController3s: UIViewController {
     
     // The Flashcard that the user have picked from the collectionView
     var selectedFlashcard = Flashcard()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Properties for centered blurred image of puzzle 1
         fullImagePuzzleThree.image = UIImage(named: selectedFlashcard.image!)
         fullImagePuzzleThree.alpha = 0.2
@@ -58,15 +58,15 @@ class PuzzleViewController3s: UIViewController {
         maskBottomLeft.image = UIImage(named: "puzzle-3pc-3")
         maskBottomLeft.frame = bottomLeftPuzzlePiece.bounds
         bottomLeftPuzzlePiece.mask = maskBottomLeft
-    
+        
         //Invisible Buttton behind the right puzzle Piece that triggers animation
         rightPuzzlePieceButton = UIButton()
         rightPuzzlePieceButton.frame.size.width = rightPuzzlePiece.frame.size.width
         rightPuzzlePieceButton.frame.size.height = rightPuzzlePiece.frame.size.height
         rightPuzzlePieceButton.center = rightPuzzlePiece.center
         //Adding Border for testing purpose, uncomment for seing size
-//        rightPuzzlePieceButton.layer.borderWidth = 3
-//        rightPuzzlePieceButton.layer.borderColor = UIColor.blue.cgColor
+        //        rightPuzzlePieceButton.layer.borderWidth = 3
+        //        rightPuzzlePieceButton.layer.borderColor = UIColor.blue.cgColor
         rightPuzzlePieceButton.addTarget(self, action: #selector(self.tappedRightPuzzlePiece(sender:)), for: .touchUpInside)
         view.addSubview(rightPuzzlePieceButton)
         
@@ -76,142 +76,144 @@ class PuzzleViewController3s: UIViewController {
         upperLeftPuzzlePieceButton.frame.size.height = upperLeftPuzzlePiece.frame.size.height
         upperLeftPuzzlePieceButton.center = upperLeftPuzzlePiece.center
         //Adding Border for testing purpose, uncomment for seing size
-//        upperLeftPuzzlePieceButton.layer.borderWidth = 3
-//        upperLeftPuzzlePieceButton.layer.borderColor = UIColor.blue.cgColor
+        //        upperLeftPuzzlePieceButton.layer.borderWidth = 3
+        //        upperLeftPuzzlePieceButton.layer.borderColor = UIColor.blue.cgColor
         upperLeftPuzzlePieceButton.addTarget(self, action: #selector(self.tappedUpperLeftPuzzlePiece(sender:)), for: .touchUpInside)
         view.addSubview(upperLeftPuzzlePieceButton)
         
-         //Invisible Buttton behind the right puzzle Piece that triggers animation
+        //Invisible Buttton behind the right puzzle Piece that triggers animation
         bottomLeftPuzzlePieceButton = UIButton()
         bottomLeftPuzzlePieceButton.frame.size.width = bottomLeftPuzzlePiece.frame.size.width
         bottomLeftPuzzlePieceButton.frame.size.height = bottomLeftPuzzlePiece.frame.size.height
         bottomLeftPuzzlePieceButton.center = bottomLeftPuzzlePiece.center
         //Adding Border for testing purpose
-//        bottomLeftPuzzlePieceButton.layer.borderWidth = 3
-//        bottomLeftPuzzlePieceButton.layer.borderColor = UIColor.blue.cgColor
+        //        bottomLeftPuzzlePieceButton.layer.borderWidth = 3
+        //        bottomLeftPuzzlePieceButton.layer.borderColor = UIColor.blue.cgColor
         bottomLeftPuzzlePieceButton.addTarget(self, action: #selector(self.tappedBotttomLeftPuzzlePiece(sender:)), for: .touchUpInside)
         view.addSubview(bottomLeftPuzzlePieceButton)
         
     }
     
-        // Animation function for right puzzle piece get triggered en tapping button
-        @objc func tappedRightPuzzlePiece(sender: UIButton) {
-                        
-                        sender.isHidden = true
-                    
-                        UIView.animate(withDuration: 2, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 5, options: [], animations:  {
-
-                        switch self.currentAnimation {
-                        case 0:
-                            
-                            // This merges two animations scale & move
-                            var concatinatedAnimation = CGAffineTransform.identity
-                            concatinatedAnimation = concatinatedAnimation.scaledBy(x: 2, y: 2)
-                            concatinatedAnimation = concatinatedAnimation.translatedBy(x: -132, y: -6)
-                            self.rightPuzzlePiece.transform = concatinatedAnimation
-
-                        case 1:
-                            self.rightPuzzlePiece.transform = .identity
-                            
-                        default:
-                            break
-                            }
-                        }) { (finished) in
-                            print("Test")
-                            
-    //                        Uncomment for testing animation several times in arow
-                            sender.isHidden = false
-
-                        }
-                        
-                        currentAnimation += 1
-                        
-                        if currentAnimation > 1 {
-                            currentAnimation = 0
-                        }
+    // Animation function for right puzzle piece get triggered en tapping button
+    @objc func tappedRightPuzzlePiece(sender: UIButton) {
+        
+        sender.isHidden = true
+        
+        UIView.animate(withDuration: 2, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 5, options: [], animations:  {
+            
+            switch self.currentAnimation {
+            case 0:
+                
+                // This merges two animations scale & move
+                var concatinatedAnimation = CGAffineTransform.identity
+                concatinatedAnimation = concatinatedAnimation.scaledBy(x: 2, y: 2)
+                concatinatedAnimation = concatinatedAnimation.translatedBy(x: -132, y: -6)
+                self.rightPuzzlePiece.transform = concatinatedAnimation
+                
+                selectedFlashcard.
+                
+            case 1:
+                self.rightPuzzlePiece.transform = .identity
+                
+            default:
+                break
             }
-    
-         // Animation function for upper left puzzle piece get triggered en tapping button
-        @objc func tappedUpperLeftPuzzlePiece(sender: UIButton) {
-                            
-                            sender.isHidden = true
-                        
-                            UIView.animate(withDuration: 2, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 5, options: [], animations:  {
-
-                            switch self.currentAnimation {
-                            case 0:
-                                // This merges two animations scale & move
-                                var concatinatedAnimation = CGAffineTransform.identity
-                                concatinatedAnimation = concatinatedAnimation.scaledBy(x: 2, y: 2)
-                                concatinatedAnimation = concatinatedAnimation.translatedBy(x: 132, y: 31)
-                                self.upperLeftPuzzlePiece.transform = concatinatedAnimation
-
-                            case 1:
-                                self.upperLeftPuzzlePiece.transform = .identity
-                                
-                            default:
-                                break
-                                }
-                            }) { (finished) in
-                                print("Test")
-                                
-        //                        Uncomment for testing animation several times in arow
-                                sender.isHidden = false
-
-                            }
-                            
-                            currentAnimation += 1
-                            
-                            if currentAnimation > 1 {
-                                currentAnimation = 0
-                            }
-                }
-    
-     // Animation function for bottom left puzzle piece get triggered en tapping button
-    @objc func tappedBotttomLeftPuzzlePiece(sender: UIButton) {
-                        
-                        sender.isHidden = true
-                    
-                        UIView.animate(withDuration: 2, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 5, options: [], animations:  {
-
-                        switch self.currentAnimation {
-                        case 0:
-                            // This merges two animations scale & move
-                            var concatinatedAnimation = CGAffineTransform.identity
-                            concatinatedAnimation = concatinatedAnimation.scaledBy(x: 2, y: 2)
-                            concatinatedAnimation = concatinatedAnimation.translatedBy(x: 131, y: -43)
-                            self.bottomLeftPuzzlePiece.transform = concatinatedAnimation
-
-                        case 1:
-                            self.bottomLeftPuzzlePiece.transform = .identity
-                            
-                        default:
-                            break
-                            }
-                        }) { (finished) in
-                            print("Test")
-                            
-    //                        Uncomment for testing animation several times in arow
-                            sender.isHidden = false
-
-                        }
-                        
-                        currentAnimation += 1
-                        
-                        if currentAnimation > 1 {
-                            currentAnimation = 0
-                        }
-            }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        }) { (finished) in
+            print("Test")
+            
+            //                        Uncomment for testing animation several times in arow
+            sender.isHidden = false
+            
+        }
+        
+        currentAnimation += 1
+        
+        if currentAnimation > 1 {
+            currentAnimation = 0
+        }
     }
-    */
-
+    
+    // Animation function for upper left puzzle piece get triggered en tapping button
+    @objc func tappedUpperLeftPuzzlePiece(sender: UIButton) {
+        
+        sender.isHidden = true
+        
+        UIView.animate(withDuration: 2, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 5, options: [], animations:  {
+            
+            switch self.currentAnimation {
+            case 0:
+                // This merges two animations scale & move
+                var concatinatedAnimation = CGAffineTransform.identity
+                concatinatedAnimation = concatinatedAnimation.scaledBy(x: 2, y: 2)
+                concatinatedAnimation = concatinatedAnimation.translatedBy(x: 132, y: 31)
+                self.upperLeftPuzzlePiece.transform = concatinatedAnimation
+                
+            case 1:
+                self.upperLeftPuzzlePiece.transform = .identity
+                
+            default:
+                break
+            }
+        }) { (finished) in
+            print("Test")
+            
+            //                        Uncomment for testing animation several times in arow
+            sender.isHidden = false
+            
+        }
+        
+        currentAnimation += 1
+        
+        if currentAnimation > 1 {
+            currentAnimation = 0
+        }
+    }
+    
+    // Animation function for bottom left puzzle piece get triggered en tapping button
+    @objc func tappedBotttomLeftPuzzlePiece(sender: UIButton) {
+        
+        sender.isHidden = true
+        
+        UIView.animate(withDuration: 2, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 5, options: [], animations:  {
+            
+            switch self.currentAnimation {
+            case 0:
+                // This merges two animations scale & movea
+                var concatinatedAnimation = CGAffineTransform.identity
+                concatinatedAnimation = concatinatedAnimation.scaledBy(x: 2, y: 2)
+                concatinatedAnimation = concatinatedAnimation.translatedBy(x: 131, y: -43)
+                self.bottomLeftPuzzlePiece.transform = concatinatedAnimation
+                
+            case 1:
+                self.bottomLeftPuzzlePiece.transform = .identity
+                
+            default:
+                break
+            }
+        }) { (finished) in
+            print("Test")
+            
+            //                        Uncomment for testing animation several times in arow
+            sender.isHidden = false
+            
+        }
+        
+        currentAnimation += 1
+        
+        if currentAnimation > 1 {
+            currentAnimation = 0
+        }
+    }
+    
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
