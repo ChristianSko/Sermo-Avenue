7bdaf9b0f8be116770bbb25537a0634879480c73
Ivan Tilev <iivantilev@gmail.com>
2020-05-27 01:42:18 +0200

    Adjusting the FindActivityViewController to update the corresponding imageView that the user has tapped

--- AlphaBeez/FindActivityViewController.swift
+++ AlphaBeez/FindActivityViewController.swift
@@ -10,13 +10,24 @@ import UIKit
 
 class FindActivityViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
-    
-    
+    // Outlets for the label
     @IBOutlet weak var textureLabel: UILabel!
     @IBOutlet weak var colorLabel: UILabel!
     @IBOutlet weak var shapeLabel: UILabel!
     
-    var currentImageView: UIImageView? = nil
+    // Outlets for the imageViews
+    @IBOutlet weak var shapeImageView: UIImageView!
+    @IBOutlet weak var colorImageView: UIImageView!
+    @IBOutlet weak var textureImageView: UIImageView!
+    
+    // Outlets of the Gesture regonizers
+    @IBOutlet var shapeGesture: UITapGestureRecognizer!
+    @IBOutlet var colorGesture: UITapGestureRecognizer!
+    @IBOutlet var textureGesture: UITapGestureRecognizer!
+    
+    
+    
+    var whatImageView = 0
     
     var imagePicker = UIImagePickerController()
     
@@ -24,7 +35,7 @@ class FindActivityViewController: UIViewController, UIImagePickerControllerDeleg
     
     // Back Button Image
     let backButton = UIImage(named: "back")
-
+    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         // Changing the native back button with our custom one
@@ -37,7 +48,7 @@ class FindActivityViewController: UIViewController, UIImagePickerControllerDeleg
     override func viewDidLoad() {
         super.viewDidLoad()
         
-//        text label
+        //        text label
         let label = UILabel(frame: CGRect(x: 0, y: 0, width: 1000, height: 1000))
         label.center = CGPoint(x: 414, y: 80)
         label.textAlignment = .center
@@ -46,50 +57,67 @@ class FindActivityViewController: UIViewController, UIImagePickerControllerDeleg
         label.font = FontKit.roundedFont(ofSize: 25, weight: .regular)
         self.view.addSubview(label)
         
-        print(selectedFlashcard.name!)
         //        show picker
         imagePicker.delegate = self
-                
-        //        enable the editing of picked image (Why? since the phrase cards are squared, the image should fits them)
-                imagePicker.allowsEditing = true
         
-//         set font
-            shapeLabel   .font = FontKit.roundedFont(ofSize: 15, weight: .regular)
-            colorLabel   .font = FontKit.roundedFont(ofSize: 15, weight: .regular)
-            textureLabel .font = FontKit.roundedFont(ofSize: 15, weight: .regular)
+        //        enable the editing of picked image (Why? since the phrase cards are squared, the image should fits them)
+        imagePicker.allowsEditing = true
+        
+        //         set font
+        shapeLabel   .font = FontKit.roundedFont(ofSize: 15, weight: .regular)
+        colorLabel   .font = FontKit.roundedFont(ofSize: 15, weight: .regular)
+        textureLabel .font = FontKit.roundedFont(ofSize: 15, weight: .regular)
     }
     
-//    update the square image view with the image picked (from gallery or camera)
+    //    update the square image view with the image picked (from gallery or camera)
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-
-          let pickedImage = info[UIImagePickerController.InfoKey.editedImage] as? UIImage
-          currentImageView?.image = pickedImage
-          currentImageView?.layer.borderWidth = 20
-          currentImageView?.layer.cornerRadius = 20
-          dismiss(animated: true, completion: nil)
-      }
+        
+        let pickedImage = info[UIImagePickerController.InfoKey.editedImage] as? UIImage
+        if whatImageView == 1 {
+            shapeImageView.image = pickedImage
+        } else if whatImageView == 2 {
+            colorImageView.image = pickedImage
+        } else if whatImageView == 3 {
+            textureImageView.image = pickedImage
+        }
+        dismiss(animated: true, completion: nil)
+    }
     
     @IBAction func tap(_ sender: UITapGestureRecognizer) {
         
-            //        create the alert with two buttons
-                       let alert = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
-               
-           //        first button: choose photo from iPhone gallery
-                           alert.addAction(UIAlertAction(title: "Photo Gallery", style: .default, handler: { (button) in
-                               self.imagePicker.sourceType = .photoLibrary
-                               self.present(self.imagePicker, animated: true, completion: nil)
-                           }))
-           //
-           //             second button: take photo with default camera
-                           alert.addAction(UIAlertAction(title: "Camera", style: .default, handler: { (button) in
-                               self.imagePicker.sourceType = .camera
-                               self.present(self.imagePicker, animated: true, completion: nil)
-                           }))
-               
-           //        Cancel\leave action
-                           alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
-               
-                           present(alert, animated: true, completion: nil)
+        let gesture = sender
+        
+        switch gesture {
+        case shapeGesture:
+            whatImageView = 1
+        case colorGesture:
+            whatImageView = 2
+        case textureGesture:
+            whatImageView = 3
+        default:
+            break
+        }
+        
+        
+        // create the alert with two buttons
+        let alert = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        
+        // first button: choose photo from iPhone gallery
+        alert.addAction(UIAlertAction(title: "Photo Gallery", style: .default, handler: { (button) in
+            self.imagePicker.sourceType = .photoLibrary
+            self.present(self.imagePicker, animated: true, completion: nil)
+        }))
+        
+        // second button: take photo with default camera
+        alert.addAction(UIAlertAction(title: "Camera", style: .default, handler: { (button) in
+            self.imagePicker.sourceType = .camera
+            self.present(self.imagePicker, animated: true, completion: nil)
+        }))
+        
+        // Cancel\leave action
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        
+        present(alert, animated: true, completion: nil)
     }
     
 }
