7bd9391b05948bc13d4bfac2dce731c0906dd791
nadiiaruocco <minimalblackcases@gmail.com>
2020-05-26 11:56:58 +0200

    Colours scheme

---
+++ AlphaBeez/ColourStruct.swift
@@ -0,0 +1,28 @@
+//
+//  ColourStruct.swift
+//  AlphaBeez
+//
+//  Created by Nadia Ruocco on 26/05/2020.
+//  Copyright © 2020 Ivan Tilev. All rights reserved.
+//
+
+import UIKit
+
+public extension UIColor {
+    
+//    market = blue
+    static var marketColor: UIColor {
+        return UIColor(red:38, green:184, blue:255, alpha:1.00)
+    }
+
+//    park = red
+    static var parkColor: UIColor {
+        return UIColor(red:242, green:46, blue:37, alpha:1.00)
+    }
+    
+//    home = purple
+    static var homeColor: UIColor {
+        return UIColor(red:128, green:48, blue:255, alpha:1.00)
+    }
+}
+
