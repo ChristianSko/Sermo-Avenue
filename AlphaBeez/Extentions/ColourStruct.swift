e3b7852de382133b334a73f9e000a9aa6925c239
Christian Skorobogatow <skorome@icloud.com>
2020-06-09 16:37:48 +0200

    added custom green and red to color struct

--- AlphaBeez/Extentions/ColourStruct.swift
+++ AlphaBeez/Extentions/ColourStruct.swift
@@ -24,5 +24,16 @@ public extension UIColor {
     static var homeColor: UIColor {
         return UIColor(red:128/255, green:48/255, blue:255/255, alpha:1.00)
     }
+    
+//    Info Alert Border & Restart puzzle Button
+    static var greenElements: UIColor {
+        return UIColor(red:70/255, green:190/255, blue:102/255, alpha:1.00)
+    }
+    
+// for the X and the back button
+    static var redElements: UIColor {
+        return UIColor(red:245/255, green:94/255, blue:87/255, alpha:1.00)
+    }
+    
 }
 
